4e386fb38c11a6714ceca562e6da33b1045ca98c
lks999 <leefg999@naver.com>
2022-06-13 00:58:05 +0900

    강의조회jsp추가

---
+++ src/main/java/studentrecord/controller/StudentRecordServlet.java
@@ -0,0 +1,24 @@
+package studentrecord.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentRecordServlet
+ */
+@WebServlet("/studentrecord")
+public class StudentRecordServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/student/studentrecord.jsp").forward(request, response);
+	}
+
+}
