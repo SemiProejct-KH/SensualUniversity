8284b0202d6aa2a0fc6d314bcf42a0d19a538fba
ymiru0324 <ymiru0324@naver.com>
2022-06-17 11:36:06 +0900

    [fix] 관리자 페이지 목록 수정

--- src/main/java/member/model/dto/MemberExt.java
+++ src/main/java/member/model/dto/MemberExt.java
@@ -1,5 +1,7 @@
 package member.model.dto;
 
+import java.sql.Date;
+
 public class MemberExt  extends Member{
 	private String departmentName;
 
@@ -15,6 +17,19 @@ public class MemberExt  extends Member{
 	@Override
 	public String toString() {
 		return "MemberExt [departmentName=" + departmentName + ", toString()=" + super.toString() + "]";
+	}
+
+	public MemberExt() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MemberExt(int memberNo, String departmentNo, String memberId, String memberPw, String memberName,
+			Date memberBirth, String memberPhone, String memberEmail, String memberImg, MemberRole memberRole,
+			String memberLevel, Date enrollDate) {
+		super(memberNo, departmentNo, memberId, memberPw, memberName, memberBirth, memberPhone, memberEmail, memberImg,
+				memberRole, memberLevel, enrollDate);
+		// TODO Auto-generated constructor stub
 	}
 	
 }
