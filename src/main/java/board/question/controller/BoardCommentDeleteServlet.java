070c49e43368eae2ab7a37d7f1307e3a38d66cdb
Class <room4072@gmail.com>
2022-06-20 14:29:25 +0900

    하늘씨 풀해주세요

---
+++ src/main/java/board/question/controller/BoardCommentDeleteServlet.java
@@ -0,0 +1,37 @@
+package board.question.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.model.service.QuestionService;
+
+/**
+ * Servlet implementation class BoardCommentDeleteServlet
+ */
+@WebServlet("/board/boardCommentDelete")
+public class BoardCommentDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private QuestionService questionService = new QuestionService();
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+			int commentNo = Integer.parseInt(request.getParameter("commentNo"));
+			//2. 비지니스로직 호출
+			int result = questionService.deleteBoardComment(commentNo);		
+			//3. 리다이렉트
+			request.getSession().setAttribute("msg", "댓글 삭제 성공!");
+			response.sendRedirect(request.getContextPath() + "/board/questionView?no=" + boardNo);
+			
+;		} catch(Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+}
